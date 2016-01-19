675d8ac206b60133dba2dd4dced003e427a50fd2
Ravi Janjam <ravi.k.janjam@vanderbilt.edu>
2016-01-19 17:06:48 -0600

    Analyzer for CMSSW_5_3_26

---
+++ RpPb/RpPb/src/pPbAnalyzer_v1.cc
@@ -0,0 +1,202 @@
+
+// system include files
+#include <memory>
+#include <algorithm>
+
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/LuminosityBlock.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+// ROOT Headers
+#include <TH1.h>
+#include <TH2.h>
+#include <TH3.h>
+#include <TGraph.h>
+#include <TF1.h>
+
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+#include <DataFormats/VertexReco/interface/Vertex.h>
+#include <DataFormats/VertexReco/interface/VertexFwd.h> 
+#include <DataFormats/TrackReco/interface/Track.h>
+#include <DataFormats/TrackReco/interface/TrackFwd.h>
+#include <DataFormats/HepMCCandidate/interface/GenParticle.h>
+#include <DataFormats/HepMCCandidate/interface/GenParticleFwd.h>
+#include "DataFormats/PatCandidates/interface/Jet.h"
+
+#include "SimDataFormats/GeneratorProducts/interface/HepMCProduct.h"
+#include "SimDataFormats/TrackingAnalysis/interface/TrackingParticle.h"
+#include "SimGeneral/HepPDTRecord/interface/ParticleDataTable.h"
+#include "SimDataFormats/TrackingAnalysis/interface/TrackingParticleFwd.h"
+#include "HepPDT/ParticleID.hh"
+#include "RecoJets/JetAlgorithms/interface/JetAlgoHelper.h"
+#include "HepMC/HeavyIon.h"
+
+using namespace std;
+using namespace edm;
+using namespace reco;
+
+class pPbAnalyzer : public edm::EDAnalyzer {
+   public:
+      explicit pPbAnalyzer(const edm::ParameterSet&);
+      ~pPbAnalyzer();
+
+       static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+       static bool vtxSort( const reco::Vertex &  a, const reco::Vertex & b );                   void initHistos(const edm::Service<TFileService> & fs);
+
+
+   private:
+      virtual void beginJob() ;
+      virtual void analyze(const edm::Event&, const edm::EventSetup&);
+      virtual void endJob() ;
+
+      virtual void beginRun(edm::Run const&, edm::EventSetup const&);
+      virtual void endRun(edm::Run const&, edm::EventSetup const&);
+      virtual void beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
+      virtual void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
+
+      // ----------member data ---------------------------
+      // ----------member data ---------------------------
+      edm::InputTag trackSrc_;
+      edm::InputTag vertexSrc_;
+      std::map<std::string,TH1F*> evtPerf_;
+      double vertexZMax_;
+	edm::Service<TFileService> fs;
+
+	// Declaring variables for passing variables to constructor to the parameter set
+	std::vector<double> ptBins_;
+	std::vector<double> etaBins_;
+	bool applyCuts_;
+	reco::TrackBase::TrackQuality trackQuality_;
+   	double trackCharge;
+	
+	TH1F *hvtxPerfX, *hvtxPerfY, *hvtxPerfZ, 
+	     *htrackPosEta, *htrackPosPt, *htrackNegEta, *htrackNegPt, 
+	     *htrackpT, *htrackEta, 
+	     *htracksPerEvent, *hprimaryVerticesPerEvent;
+
+	double nPosChargedParticles, nNegChargedParticles;
+
+
+	/* Variable used inside Analyze method */
+	double vxErr, vyErr, vzErr;
+	double dxy, dz, dxysigma, dzsigma;
+	double nTracksWithQualityCut,  nTracksWithoutQualityCut; 
+	double trackPt, trackEta;
+
+};
+
+//
+// constants, enums and typedefs
+//
+
+//
+// static data member definitions
+//
+
+//
+// constructors and destructor
+//
+pPbAnalyzer::pPbAnalyzer(const edm::ParameterSet& iConfig)
+:trackSrc_(iConfig.getParameter<edm::InputTag>("trackSrc")),
+vertexSrc_(iConfig.getParameter<edm::InputTag>("vertexSrc")),
+vertexZMax_(iConfig.getParameter<double>("vertexZMax")),
+ptBins_(iConfig.getParameter<std::vector<double> >("ptBins")),
+etaBins_(iConfig.getParameter<std::vector<double> >("etaBins")),
+applyCuts_(iConfig.getParameter<bool>("applyCuts"))
+{
+   //now do what ever initialization is needed
+   //
+        //usesResource("TFileService");
+	initHistos(fs);
+	trackQuality_ = TrackBase::qualityByName(iConfig.getParameter<std::string>("TrackQuality"));
+
+
+}
+
+
+pPbAnalyzer::~pPbAnalyzer()
+{
+ 
+   // do anything here that needs to be done at desctruction time
+   // (e.g. close files, deallocate resources etc.)
+
+}
+
+
+//
+// member functions
+//
+
+// ------------ method called for each event  ------------
+void
+pPbAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
+{
+   using namespace edm;
+
+
+
+#ifdef THIS_IS_AN_EVENT_EXAMPLE
+   Handle<ExampleData> pIn;
+   iEvent.getByLabel("example",pIn);
+#endif
+   
+#ifdef THIS_IS_AN_EVENTSETUP_EXAMPLE
+   ESHandle<SetupData> pSetup;
+   iSetup.get<SetupRecord>().get(pSetup);
+#endif
+}
+
+void pPbAnalyzer::initHistos(const edm::Service<TFileService> & fs){}
+
+// ------------ method called once each job just before starting event loop  ------------
+void 
+pPbAnalyzer::beginJob()
+{
+}
+
+// ------------ method called once each job just after ending the event loop  ------------
+void 
+pPbAnalyzer::endJob() 
+{
+}
+
+// ------------ method called when starting to processes a run  ------------
+void 
+pPbAnalyzer::beginRun(edm::Run const&, edm::EventSetup const&)
+{
+}
+
+// ------------ method called when ending the processing of a run  ------------
+void 
+pPbAnalyzer::endRun(edm::Run const&, edm::EventSetup const&)
+{
+}
+
+// ------------ method called when starting to processes a luminosity block  ------------
+void 
+pPbAnalyzer::beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
+{
+}
+
+// ------------ method called when ending the processing of a luminosity block  ------------
+void 
+pPbAnalyzer::endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
+{
+}
+
+// ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
+void
+pPbAnalyzer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  //The following says we do not know what parameters are allowed so do no validation
+  // Please change this to state exactly what you do use, even if it is no parameters
+  edm::ParameterSetDescription desc;
+  desc.setUnknown();
+  descriptions.addDefault(desc);
+}
+
+//define this as a plug-in
+DEFINE_FWK_MODULE(pPbAnalyzer);
