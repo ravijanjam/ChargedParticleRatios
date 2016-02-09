b33517bc861889f8592ea95ef9d57a8c28e6f4b1
Ravi Janjam <ravi.k.janjam@vanderbilt.edu>
2016-02-09 11:52:06 -0600

    pPb Analysis macros

---
+++ RpPb/test/pPbConfigs/crab3Config_pPb.py
@@ -0,0 +1,36 @@
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+
+
+''' timeStamp the outputfile from cmsRun '''
+
+import datetime, time
+
+ts = time.time()
+timeLabel = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d-%H%M%S')
+
+timeStampedFolderName = "pPbAnalyzerFull" + timeLabel 
+
+
+''' ==================================== '''
+
+config = config()
+
+config.General.requestName = timeStampedFolderName 
+config.General.workArea = 'pPb_crab_projects'
+config.General.transferOutputs = True
+config.General.transferLogs = False
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'pPbAnalysis_cfg.py'
+
+config.Data.inputDataset = '/PAHighPt/HIRun2013-28Sep2013-v1/RECO'
+config.Data.inputDBS = 'global'
+config.Data.splitting = 'LumiBased'
+config.Data.unitsPerJob = 20
+config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions13/pPb/Prompt/Cert_210498-211631_HI_PromptReco_Collisions13_JSON_v2.txt'
+config.Data.runRange = '210498-211631' # '193093-194075'
+config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
+config.Data.publication = True
+config.Data.outputDatasetTag = 'RpPb_PAHighPt_Sept2013'
+
+config.Site.storageSite = 'T2_US_Vanderbilt'
