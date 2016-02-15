4fb50199edecbe8b977179adba5110cacf6bd005
bokadia <bokadia@localhost.localdomain>
2016-02-15 14:08:39 -0500

    Rebinning errors + normalization

--- HistogramRatioPlots/removeRootJunk.sh
+++ HistogramRatioPlots/removeRootJunk.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-fileExt=( *.pcm *.so *.d)
+fileExt=( pcm so d )
 echo "Do you want to delete?"
 echo "The following extensions will be deleted" ${fileExt[@]}
 
 for i in ${fileExt[@]}
 do 
-	echo "Deleting the files:  $i"
-	rm $i;
+	echo "Deleting the files: ---x `ls *.$i`"
+	rm *.$i;
 done
--- HistogramRatioPlots/variableBinning_v1.C
+++ HistogramRatioPlots/variableBinning_v1.C
@@ -1,11 +1,53 @@
+
+// C++ Headers
+#include <iostream>
+
+// ROOT Headers
 #include <TH1D.h>
 #include <TFile.h>
+#include <TCanvas.h>
 
-void variableBinning(){
+using namespace std;
 
-	TFile *f = new TFile(/*Name of the file*/);
-	TH1D *hist;
+void variableBinning_v1(char *inFile){
+
+	TCanvas *cOrig = new TCanvas("cOrig", "", 700, 400);
+	TCanvas *cRebin = new TCanvas("cRebin", "", 700, 400);
+
+	TFile *f = new TFile(inFile);
 	
-	hist = (TH1D*)f->Get(/*Path*/);
-	hist->Draw();
+	// Initiate histograms
+	TH1D *hPosPt, *hNegPt;
+	
+	char *sPosPt, *sNegPt;
+ 	sPosPt = "/userAnalyzer/htrackNegPt";
+        sNegPt = "/userAnalyzer/htrackPosPt";
+
+	hPosPt = (TH1D*)f->Get(sPosPt);
+	hNegPt = (TH1D*)f->Get(sNegPt);
+
+	double binArr[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 50, 90, 130, 180};
+	int nBins = sizeof(binArr)/sizeof(double)-1;
+
+	TH1D *hPosVarBin;
+	hPosVarBin = (TH1D*)hPosPt->Clone();
+	hPosVarBin = (TH1D*)hPosVarBin->Rebin(nBins, "hPosVarBin", binArr);	
+
+
+	cout << "=======================================" << endl;
+	cout << "Number of bins in original histogram: " 
+	     << hPosPt->GetNbinsX()
+	     << endl;
+
+	cout << "Number of bins in rebinned histogram: " 
+	     << hPosVarBin->GetNbinsX()
+	     << endl;
+	cout << "=======================================" << endl;
+	
+
+	cOrig->cd();
+	hPosPt->Draw();
+	
+	cRebin->cd();
+	hPosVarBin->Draw();
 }
